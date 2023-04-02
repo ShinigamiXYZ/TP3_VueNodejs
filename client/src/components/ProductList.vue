415119c03407af63089bd56f4a02f196c545ab72
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-04-02 14:28:56 -0400

    setting project environnement

---
+++ client/src/components/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+      <table class="table table-striped table-bordered">
+        <thead>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Type</th>
+        </thead>
+        <tr v-for="product in products" :key="product._id">
+          <td>{{ product.name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.description }}</td>
+          <td>{{ product.type }}</td>
+        </tr>
+      </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:8080/api/product')
+      .then(response => {
+        this.products = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
